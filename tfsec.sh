02ca217165587a5cd3cf0c1802f0ec6542c5a406
Jaime Silva <jaime.silva@gruposantander.com>
2021-11-02 11:09:32 +0100

    upload actions fix path needed 1

--- tfsec.sh
+++ tfsec.sh
@@ -2,28 +2,28 @@
 
 set -x
 
-if [ -n "${GITHUB_WORKSPACE}" ]; then
-  cd "${GITHUB_WORKSPACE}" || exit
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  cd "$GITHUB_WORKSPACE" || exit
 fi
 
 VERSION="latest"
 if [ "$INPUT_TFSEC_VERSION" != "latest" ]; then
-  VERSION="tags/${INPUT_TFSEC_VERSION}"
+  VERSION="tags/$INPUT_TFSEC_VERSION"
 fi
 
 # Download the required tfsec version
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/${VERSION} -O - | grep -o -E "https://.+?tfsec-linux-amd64" | head -n1)" >tfsec
+wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/$VERSION -O - | grep -o -E "https://.+?tfsec-linux-amd64" | head -n1)" >tfsec
 install tfsec /usr/local/bin/tfsec
 
-if [ -n "${INPUT_TFVARS_FILE}" ]; then
-  echo "::debug::Using tfvars file ${INPUT_TFVARS_FILE}"
-  TFVARS_OPTION="--tfvars-file ${INPUT_TFVARS_FILE}"
+if [ -n "$INPUT_TFVARS_FILE" ]; then
+  echo "::debug::Using tfvars file $INPUT_TFVARS_FILE"
+  TFVARS_OPTION="--tfvars-file $INPUT_TFVARS_FILE"
 fi
 
-echo {} >${INPUT_SARIF_FILE}
+echo {} >$INPUT_SARIF_FILE
 
-tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" ${TFVARS_OPTION} >${INPUT_SARIF_FILE}
+tfsec --format=sarif "$INPUT_WORKING_DIRECTORY" $TFVARS_OPTION >$INPUT_SARIF_FILE
 
-tfsec_return="${PIPESTATUS[0]}" exit_code=$?
+tfsec_return="$PIPESTATUS[0]" exit_code=$?
 
-echo ::set-output name=tfsec-return-code::"${tfsec_return}"
+echo ::set-output name=tfsec-return-code::"$tfsec_return"
